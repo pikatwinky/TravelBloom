710711c7b20687348aa213a149b9bf8c349a9cb9
pikatwinky <pikatwinky.crimson@gmail.com>
2024-09-15 23:56:44 -0400

    Testing the use of JSON Keys for search functionality

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,5 +2,18 @@ document.getElementById("searchBtn").addEventListener('click',printMessage);
 document.getElementById("resetBtn").addEventListener('click',printMessage);
 
 function printMessage(){
-    console.log("BUSQUEDA");
+    let input=document.getElementById('searchField').value.toLowerCase();
+    const lastChar=input.charAt(input.length-1);
+    if (lastChar==='s'){
+        console.log("El último caracter es una s");
+        input=input.substring(0,input.length-1);
+        console.log(input);
+    }
+    fetch('./travel_recommendation_api.json')
+    .then(response=>response.json() )
+    .then(data => {
+        console.log(Object.keys(data).indexOf(input) );
+ //       console.log(data[input]);
+    });
+    
 }
